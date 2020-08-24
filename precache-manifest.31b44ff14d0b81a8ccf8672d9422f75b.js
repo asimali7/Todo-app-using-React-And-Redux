f67b3b5c35f58d4b70227ddc01290746ff73a329
asimali7 <33003365+asimali7@users.noreply.github.com>
2020-08-24 15:30:26 +0500

    Updates

---
+++ precache-manifest.31b44ff14d0b81a8ccf8672d9422f75b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23f2c2211e9226d56d81f0a23d46669f",
+    "url": "/Todo-app-using-React-And-Redux/index.html"
+  },
+  {
+    "revision": "0097a009edfbdebea4bc",
+    "url": "/Todo-app-using-React-And-Redux/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "0097a009edfbdebea4bc",
+    "url": "/Todo-app-using-React-And-Redux/static/js/2.b8059ca3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Todo-app-using-React-And-Redux/static/js/2.b8059ca3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17c9b54001034889a494",
+    "url": "/Todo-app-using-React-And-Redux/static/js/main.5d0955f0.chunk.js"
+  },
+  {
+    "revision": "91cf762d30bdeaa14dd6",
+    "url": "/Todo-app-using-React-And-Redux/static/js/runtime-main.d3907745.js"
+  }
+]);
